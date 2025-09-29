ae0269226cad52ef4cc4e40396efae742b08dfe7
Joao <gonzales.jpgc@gmail.com>
2025-09-29 11:32:31 -0300

    Declarando variavéis

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,23 @@
-alert("Hello world")
+//alert("Hello world")
+
+//DECLARAR VARIAVÉL
+
+//{} objeto
+//[] array
+//() metôdo
+
+var nome = "Fiap";
+console.log(typeof nome);
+
+let nome1 = '10';
+console.log(typeof nome1);
+
+const nome2 = {}; //Constante não pode ser alterado!
+console.log(typeof nome2); 
+
+let nome3 = [];
+console.log(typeof nome3)
+
+let nome4 = true;
+console.log(typeof nome4)
+
